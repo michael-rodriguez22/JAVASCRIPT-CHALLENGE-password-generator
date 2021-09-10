8c77deaf27e1e1131f65fddd66c4c5d7137b6f94
Michael Rodriguez <mikeyrod22@gmail.com>
2021-09-10 02:40:46 -0500

    added function to collect desired length of password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
 // Assignment code here
+function generatePassword() {
+  // ask for length of password
+  let passwordLength;
+  
+  function passwordLengthPrompt() {
+    let input = window.prompt(
+      "How many characters would you like your password to have? (input must be a whole number between 8 and 128)"
+    );
+    if (input.match(/[^0-9]/) || parseInt(input) < 8 || parseInt(input) > 128) {
+      window.alert("Error: Invalid input. Please try again.");
+      return passwordLengthPrompt();
+    } else {
+      passwordLength = parseInt(input);
+      console.log(`password length: ${passwordLength}`);
+    }
+  }
 
+  passwordLengthPrompt();
+  // ask which characters will be used
+  // return randomly generated password in accordance with user criteria
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +30,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
