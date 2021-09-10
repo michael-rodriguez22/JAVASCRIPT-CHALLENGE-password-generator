8f225544050280874342dbb753bc8a8b62ee438e
Michael Rodriguez <mikeyrod22@gmail.com>
2021-09-10 03:19:00 -0500

    completed password generating function

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,10 +20,12 @@ function generatePassword() {
 
   // ask which characters will be used
   const characterTypes = {};
-  
+
   function characterTypesPrompt() {
     function check(type) {
-      return window.confirm(`Would you like to include ${type}? Select 'OK' for yes or 'Cancel' for no.`);
+      return window.confirm(
+        `Would you like to include ${type}? Select 'OK' for yes or 'Cancel' for no.`
+      );
     }
     characterTypes.uppercase = check("uppercase letters");
     characterTypes.lowercase = check("lowercase letters");
@@ -37,14 +39,39 @@ function generatePassword() {
       console.log("character types:");
       console.dir(characterTypes);
     } else {
-      window.alert("Error: No character types included. Please include at least one character type.");
+      window.alert(
+        "Error: No character types included. Please include at least one character type."
+      );
       characterTypesPrompt();
     }
   }
 
   characterTypesPrompt();
-  
+
   // return randomly generated password in accordance with user criteria
+  let charactersString = "";
+  let password = "";
+
+  if (characterTypes.lowercase)
+    charactersString += "abcdefghijklmnopqrstuvwxyz";
+  if (characterTypes.uppercase)
+    charactersString += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (characterTypes.numbers) charactersString += "0123456789";
+  if (characterTypes.specials) {
+    charactersString += "' !#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    charactersString += '"';
+  }
+
+  for (let i = 0; i < passwordLength; i++) {
+    password += charactersString.charAt(
+      Math.floor(Math.random() * charactersString.length)
+    );
+  }
+  console.log(`password: ${password}`);
+  console.log(
+    "I hope you enjoyed this password generating experience! - Michael"
+  );
+  return password;
 }
 
 // Get references to the #generate element
